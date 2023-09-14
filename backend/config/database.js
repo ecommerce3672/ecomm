d16ba07e6fc7d2075609b4fa7c04030aae29843d
Dinesh Gupta <gupta.dk3672@gmail.com>
2023-09-14 23:19:57 +0530

    port

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDatabase = () => {
   mongoose
-    .connect(process.env.DB_URI)
+    .connect('mongodb+srv://medineshgupta:jony3672@ecommerce.2qwbcla.mongodb.net/Ecommerce?retryWrites=true&w=majority')
     .then((data) => {
       console.log(`Mongodb connected with server: ${data.connection.host}`);
     })
