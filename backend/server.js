bbaf30d5e819e7ab9563c5d248b994f4a5e90103
Dinesh Gupta <gupta.dk3672@gmail.com>
2023-09-14 22:43:10 +0530

    server

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const app = require("./app");
 const cloudinary = require('cloudinary')
+const dotenv=require("dotenv")
 
 const connectDatabase = require("./config/database");
 
@@ -13,10 +14,10 @@ process.on("uncaughtException", (err) => {
 
 // config
 
-// dotenv.config({ path: "backend/config/config.env" });
-if (process.env.NODE_ENV !== "PRODUCTION") {
-  require("dotenv").config({ path: "backend/config/config.env" });
-}
+dotenv.config({ path: "backend/config/config.env" });
+// if (process.env.NODE_ENV !== "PRODUCTION") {
+//   require("dotenv").config({ path: "backend/config/config.env" });
+// }
 
 // Connecting to database
 
